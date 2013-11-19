d2a75e8646467bffcc0c2af19d2ff67f9dd259f0
Kujbor <Kujbor@ya.ru>
2013-11-19 17:17:40 +0300

    Update log.js

--- log.js
+++ log.js
@@ -3,7 +3,7 @@
  * Author Oleg Taranov aka Kujbor
  * Copyright (C) 2013: CubeComp Development
  */
-define("jquery.log", ["jquery"], function() {
+define("jquery.log", ["jquery"], function($) {
 
     "use strict";
 
